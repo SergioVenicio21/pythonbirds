0dee94dc6622c677c6e304b052f7820612a630f7
sergio <sergionirvana2010@hotmail.com>
2018-04-28 17:50:37 -0300

    Criação do método comprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,5 +3,15 @@
 
 
 class Pessoa:
-    def __init__(self):
-        pass
+    def __init__(self, nome):
+        self.nome = nome
+
+    def comprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    nome = input('Digite seu nome: ')
+    p = Pessoa(nome)
+    print(p.comprimentar())
+    print(id(p))
