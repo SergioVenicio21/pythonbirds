2af1cd922a9857b92719e7f87bca8016f2df0283
sergio <sergionirvana2010@hotmail.com>
2018-04-28 18:09:12 -0300

    Criação do atributo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,8 +3,10 @@
 
 
 class Pessoa:
-    def __init__(self, nome):
+    def __init__(self, nome, idade, *filhos):
         self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
 
     def comprimentar(self):
         return f'Olá {id(self)}'
@@ -12,6 +14,12 @@ class Pessoa:
 
 if __name__ == '__main__':
     nome = input('Digite seu nome: ')
-    p = Pessoa(nome)
+    p2 = Pessoa('teste', 23)
+    p3 = Pessoa('teste2', 23)
+    p4 = Pessoa('teste3', 23)
+    p5 = Pessoa('teste4', 23)
+    p = Pessoa(nome, 23, p2, p3, p4, p5)
     print(p.comprimentar())
+    for filho in p.filhos:
+        print(filho.nome)
     print(id(p))
